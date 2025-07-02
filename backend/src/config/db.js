5672ecea3bd27da695112ea46df60fae0b570985
wisnu-wicaksana <wisnuwicaksana100@gmail.com>
2025-07-02 21:32:22 +0700

    fix: final setup for backend local and vercel

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,8 @@
-const { PrismaClient } = require('@prisma/client');
+// File: backend/src/config/db.js
 
-// Cek jika sudah ada instance prisma di global object, jika tidak, buat baru.
-const prisma = global.prisma || new PrismaClient();
+// ✅ PATH YANG BENAR
+const { PrismaClient } = require('../../prisma/generated/prisma');
 
-// Di lingkungan development, simpan instance yang baru dibuat ke global object.
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
+const prisma = new PrismaClient();
 
 module.exports = prisma;
