cea35a7f1eac1efa947379ae75e2c868125872a4
wisnu-wicaksana <wisnuwicaksana100@gmail.com>
2025-07-02 21:45:48 +0700

    Initial deploy setup

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,4 @@
-// File: backend/src/config/db.js
-
-// ✅ PATH YANG BENAR
-const { PrismaClient } = require('../../prisma/generated/prisma');
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
