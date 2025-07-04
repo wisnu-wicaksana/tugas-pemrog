5115e308a9e509e43d1361ac3ad85b087a3cb781
wisnu-wicaksana <wisnuwicaksana100@gmail.com>
2025-07-04 16:10:03 +0700

    update manga

--- frontend/components/manga/Manga.js
+++ frontend/components/manga/Manga.js
@@ -19,11 +19,7 @@ export default function Manga() {
 
   return (
     <main className="p-4">
-      <div className="mb-4 text-right">
-        <Link href="/" className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">
-          Kembali ke Beranda
-        </Link>
-      </div>
+     
 
       <h1 className="text-2xl font-bold mb-4">Top Manga (Preview)</h1>
 
