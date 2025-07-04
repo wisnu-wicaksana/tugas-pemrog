53bec742cdfdbfb9f59f2fd77ffd0c3030475b3d
wisnu-wicaksana <wisnuwicaksana100@gmail.com>
2025-07-04 09:48:18 +0700

    setup payment

--- backend/src/controllers/paymentSuccess.controller.js
+++ backend/src/controllers/paymentSuccess.controller.js
@@ -5,25 +5,29 @@ async function checkPaymentStatus(req, res) {
 
   if (!tripay_merchant_ref) {
     
-    res.redirect("http://localhost:3000/dashboard?status=success");
-
+    res.redirect(`${process.env.FRONTEND_URL}/dashboard?status=success`);
+    //res.redirect(`${process.env.FRONTEND_URL}/dashboard?status=success`);
   }
 
   try {
     const payment = await findPaymentByMerchantRef(tripay_merchant_ref);
 
     if (!payment) {
-      return res.redirect('http://localhost:3000/dashboard?status=notfound');
+      //return res.redirect('http://localhost:3000/dashboard?status=notfound');
+      return res.redirect(`${process.env.FRONTEND_URL}/dashboard?status=notfound`);
     }
 
     if (payment.status === 'PAID') {
-      return res.redirect('http://localhost:3000/dashboard?status=success');
+      //return res.redirect('http://localhost:3000/dashboard?status=success');
+      return res.redirect(`${process.env.FRONTEND_URL}/dashboard?status=success`);
     } else {
-      return res.redirect('http://localhost:3000/dashboard?status=pending');
+      //return res.redirect('http://localhost:3000/dashboard?status=pending');
+      return res.redirect(`${process.env.FRONTEND_URL}/dashboard?status=pending`);
     }
   } catch (error) {
     console.error('Gagal memeriksa status pembayaran:', error);
-    return res.redirect('http://localhost:3000/dashboard?status=error');
+    //return res.redirect('http://localhost:3000/dashboard?status=error');
+    return res.redirect(`${process.env.FRONTEND_URL}/dashboard?status=error`);
   }
 }
 
