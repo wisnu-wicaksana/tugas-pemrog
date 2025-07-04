6690f42ffa410fddb00adea389681cbe26111555
wisnu-wicaksana <wisnuwicaksana100@gmail.com>
2025-07-04 10:07:04 +0700

    debug  payment

--- frontend/app/payment-success/PaymentStatus.js
+++ frontend/app/payment-success/PaymentStatus.js
@@ -5,14 +5,14 @@ import { useEffect, useState } from 'react';
 // Komponen ini sekarang berisi semua logika sisi klien
 export default function PaymentStatus() {
   const searchParams = useSearchParams();
-  const reference = searchParams.get('reference');
+  const reference = searchParams.get('tripay_reference');
   const [message, setMessage] = useState('Memeriksa status pembayaran...');
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const checkStatus = async () => {
       try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/payment-success?reference=${reference}`);
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/payment-success?reference=${tripay_reference}`);
         const text = await res.text();
         setMessage(text);
       } catch (err) {
